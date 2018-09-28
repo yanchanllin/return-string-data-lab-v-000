24761a7f4d02683c02b1b4a8b483e993ee2ee55e
YanC Lin <yonalin1@gmail.com>
2018-09-28 16:50:56 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,6 +24,11 @@ class ProductsController < ApplicationController
    def inventory
    @product = Product.find(params[:id])
    render plain: @product.inventory > 0 ? true : false
+   end
+ 
+ def description
+   @product = Product.find(params[:id])
+   render plain: @product.description
  end
 
    private
