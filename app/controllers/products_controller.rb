09ce18565c7910a54d70f77bf8a1352a08d6fd24
YanC Lin <yonalin1@gmail.com>
2018-09-28 16:15:46 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,10 +3,10 @@ class ProductsController < ApplicationController
   def index
      @products = Product.all
    end
- 
+
    def new
    end
- 
+
    def create
      @product = Product.new(product_params)
      if @product.save
@@ -15,12 +15,12 @@ class ProductsController < ApplicationController
        render "new"
      end
    end
- 
+
    def body
      @product = Product.find(params[:id])
      render plain: @product.description
    end
- 
+
    def inventory
      @product = Product.find(params[:id])
      if @product.inventory > 0
@@ -30,11 +30,11 @@ class ProductsController < ApplicationController
      end
      render plain: @inventory
    end
- 
+
    private
- 
+
    def product_params
      params.require(:product).permit(:name, :description, :price, :inventory)
    end
- 
+
  end
